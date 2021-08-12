8f2d5bb1cdd2261c2085b5aaadbe92232a9f14f6
bellaappel <bellaappel0@gmail.com>
2021-08-12 09:53:02 -0700

    update params

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -46,6 +46,6 @@ class RecipesController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def recipe_params
-      params.require(:recipe).permit(:cocktail_id, :instructions, :creator)
+      params.require(:recipe).permit(:cocktail_id, :instructions, :creator, :title)
     end
 end
